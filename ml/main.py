aa4f0aa207d44138643ae36e90c1ab437326cfdb
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-11-17 18:31:44 -0700

    multithread now

--- ml/main.py
+++ ml/main.py
@@ -1,6 +1,8 @@
 from emotion_webcam import FaceAnalyzer
+import threading 
 import time
-def main():
+
+def facialEmotion():
     analyzer = FaceAnalyzer()
     analyzer.start_camera()
     for i in range(15):
@@ -8,8 +10,16 @@ def main():
         analyzer.run()
     print("\n  : ", analyzer.data)
     analyzer.stop_camera()
+def transcribeEmotion():
     speechAnalyzer = speechAnalyzer() 
     speechAnalyzer.run()
     print(speechAnalyzer.data)  
     
+    
+def main():
+    p1= threading.Thread(target=facialEmotion)
+    p2= threading.Thread(targeet= transcribeEmotion)
+    p1.start()
+    p2.start()
+
 main()
