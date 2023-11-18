6d2c02b94229ec56804319cbbcc99c0a16e7f30c
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-11-17 19:30:31 -0700

    Better implmenetations on dealing with data

--- ml/main.py
+++ ml/main.py
@@ -1,26 +1,38 @@
 from emotion_webcam import FaceAnalyzer
 from transcribe_emotion import SpeechAnalyzer
+from queue import Queue
 import threading 
 import time
 
-def facialEmotion():
+def facialEmotion(result_queue):
     analyzer = FaceAnalyzer()
     analyzer.start_camera()
     for i in range(15):
         time.sleep(1)
         analyzer.run()
-    print("\n  : ", analyzer.data)
     analyzer.stop_camera()
-def transcribeEmotion():
+    result_queue.put(analyzer.data)
+    
+    
+def transcribeEmotion(result_queue):
     speechAnalyzer = SpeechAnalyzer() 
     speechAnalyzer.run()
-    print(speechAnalyzer.data)  
+    result_queue.put(speechAnalyzer.data)   
     
     
 def main():
-    p1= threading.Thread(target=facialEmotion)
-    p2= threading.Thread(target= transcribeEmotion)
+    result_queue = Queue()
+    p1= threading.Thread(target=facialEmotion(result_queue))
+    p2= threading.Thread(target= transcribeEmotion(result_queue))
     p1.start()
     p2.start()
+    
+    p1.join()
+    p2.join()
+    facial_emotion_result = result_queue.get()
+    transcribe_emotion_result = result_queue.get()
+
+    print("Facial Emotion Result:", facial_emotion_result)
+    print("Transcribe Emotion Result:", transcribe_emotion_result)
 
 main()
