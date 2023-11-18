850e416cf43c5663998f85d58b1be4609b985427
Taekwan Yoon <taekwan@ualberta.ca>
2023-11-17 21:33:47 -0700

    regular update

--- ml/main.py
+++ ml/main.py
@@ -7,8 +7,8 @@ import time
 def facialEmotion(result_queue):
     analyzer = FaceAnalyzer()
     analyzer.start_camera()
-    for i in range(15):
-        time.sleep(1)
+    for i in range(5):
+        time.sleep(2)
         analyzer.run()
     analyzer.stop_camera()
     result_queue.put(analyzer.data)
@@ -21,16 +21,19 @@ def transcribeEmotion(result_queue):
     
     
 def main():
-    result_queue = Queue()
-    p1= threading.Thread(target=facialEmotion(result_queue))
-    p2= threading.Thread(target= transcribeEmotion(result_queue))
+    facial_queue = Queue()
+    transcribe_queue =Queue()
+    p1= threading.Thread(target=facialEmotion,args=(facial_queue,))
+    p2= threading.Thread(target= transcribeEmotion,args=(transcribe_queue,))
     p1.start()
     p2.start()
     
     p1.join()
     p2.join()
-    facial_emotion_result = result_queue.get()
-    transcribe_emotion_result = result_queue.get()
+
+
+    facial_emotion_result = facial_queue.get()
+    transcribe_emotion_result = transcribe_queue.get()
 
     print("Facial Emotion Result:", facial_emotion_result)
     print("Transcribe Emotion Result:", transcribe_emotion_result)
